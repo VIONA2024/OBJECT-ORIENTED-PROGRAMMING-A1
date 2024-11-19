6e02e8ce99ebbe1297b5edc8ff3557231fe13d53
VIONA2024 <163629305+VIONA2024@users.noreply.github.com>
2024-11-19 17:46:06 +0800

    Part 2
    Today we will continue from the first day. We already have the HealthProfessional base class, and now we want to create a GeneralPractitioner class, which is a subclass of HealthProfessional. This subclass has its own special features. It has a new instance variable isGoodAtFamilyMedicine. It also has a default constructor and a constructor that initialises all the instance variables. Here's what you need to pay attention to: when initialising the subclass instance variables, you also need to initialise the base class instance variables by calling the base class constructor via super. Then there is a method that prints the details of the general practitioner, including type, ID, name, department, and whether or not they are good at family medicine. Finally, getter and setter methods are also added. And in the main method, we briefly test creating a general practitioner object and printing the information (although this part of the test code will be further improved in the future, let's first briefly see if the creation and printing functions are working properly).

--- Main.java
+++ Main.java
@@ -46,3 +46,44 @@ class HealthProfessional {
         this.department = department;
     }
 }
+
+class GeneralPractitioner extends HealthProfessional {
+    private boolean isGoodAtFamilyMedicine;
+
+    // 这是默认构造函数，先留着备用
+    public GeneralPractitioner() {
+    }
+
+    // 这个构造函数可以初始化全科医生的所有信息，包括基类的那些哦
+    public GeneralPractitioner(int ID, String name, String department, boolean isGoodAtFamilyMedicine) {
+        super(ID, name, department);
+        this.isGoodAtFamilyMedicine = isGoodAtFamilyMedicine;
+    }
+
+    // 这个方法会打印出全科医生的详细信息，先判断ID是不是1，如果是就打印个标题，然后打印类型、调用基类的打印方法打印基类信息，最后打印是否擅长家庭医疗
+    @Override
+    public void printAllVariables() {
+        if (super.getID() == 1) {
+            System.out.println("Details of the health professional are:");
+        }
+        System.out.println("Type: General Practitioner");
+        super.printAllVariables();
+        System.out.println("Good at family medicine: " + isGoodAtFamilyMedicine);
+    }
+
+    // 下面是Getter和Setter方法，用来获取和设置是否擅长家庭医疗这个属性
+    public boolean isGoodAtFamilyMedicine() {
+        return isGoodAtFamilyMedicine;
+    }
+
+    public void setGoodAtFamilyMedicine(boolean goodAtFamilyMedicine) {
+        this.isGoodAtFamilyMedicine = goodAtFamilyMedicine;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        GeneralPractitioner gp = new GeneralPractitioner(1, "Doctor Smith", "General Practice", true);
+        gp.printAllVariables();
+    }
+}
